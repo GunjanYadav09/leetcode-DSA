9d8cb25777011829f6f96ea193b903d0c1bb85c4
GunjanYadav09 <gunjanyadav927@gmail.com>
2026-08-07 23:34:52 +0530

    Solve LeetCode 42 - Trapping Rain Water

---
+++ Two Pointers/42_Trapping_Rain_Water.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def trap(self, height):
+        left = 0
+        right = len(height)-1
+
+        lMax = 0
+        rMax = 0
+        water = 0
+
+        while left < right:
+            if height[left] <= height[right]:
+                if height[left] >= lMax:
+                    lMax = height[left]
+                else:
+                    water += lMax - height[left]
+                left += 1
+            else:
+                if height[right] >= rMax:
+                    rMax = height[right]
+                else:
+                    water += rMax - height[right]
+                right -= 1
+        return water
